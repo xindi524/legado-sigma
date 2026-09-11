195dbcb1fb6691361122815d620abb7b318c97c0
xindi524 <xindi524@gmail.com>
2026-09-12 01:17:07 +0800

    Add files via upload

--- app/src/main/java/io/legado/app/data/dao/BookGroupDao.kt
+++ app/src/main/java/io/legado/app/data/dao/BookGroupDao.kt
@@ -30,10 +30,10 @@ interface BookGroupDao {
     @get:Query(
         """
         with const as (SELECT sum(groupId) sumGroupId FROM book_groups where groupId > 0)
-        SELECT book_groups.* FROM book_groups join const 
-        where show > 0 
+        SELECT book_groups.* FROM book_groups join const
+        where show > 0
         and (
-            (groupId >= 0  and exists (select 1 from books where `group` & book_groups.groupId > 0))
+            groupId >= 0
             or groupId = -1
             or (groupId = -2 and exists (select 1 from books where type & ${BookType.local} > 0))
             or (groupId = -3 and exists (select 1 from books where type & ${BookType.audio} > 0))
